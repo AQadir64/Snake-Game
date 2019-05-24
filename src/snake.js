3d971dd0ef07734e206cd65e1c2fcfa2aae84cb7
Qadir <A.qadir67@outlook.com>
2019-05-24 05:38:24 +0500

    snake movement function

--- src/snake.js
+++ src/snake.js
@@ -29,3 +29,76 @@ let snake = [
 ]
 
 
+/////////////////////////////////////////////////////////
+//
+// Initial Snake 
+//
+////////////////////////////////////////////////////////
+
+const drawSnakePart = (snakePart) => {
+
+    const gameCanvas = document.getElementById('gameCanvas')
+    const ctx = gameCanvas.getContext('2d')
+    
+    const snakebag = "lightgreen"
+    const snakeborder = "darkgreen"
+
+    ctx.fillStyle = snakebag
+    ctx.strokestyle = snakeborder
+
+    ctx.fillRect(snakePart.x, snakePart.y, 10, 10)
+    ctx.strokeRect(snakePart.x, snakePart.y, 10, 10)
+}
+
+const drawSnake = () => {
+    snake.forEach(element => {
+        drawSnakePart(element)
+    });
+}
+
+drawSnake()
+
+/////////////////////////////////////////////////////////
+//
+// Snake move 
+// //  key to move snake 
+// // dx change in x
+// // dy chnage in y
+//
+////////////////////////////////////////////////////////
+
+
+const key = (dx=10, dy=0) => {
+    console.log(snake)
+    const head = { x: snake[0].x + dx, y: snake[0].y + dy }
+    snake.unshift(head)
+    snake.pop()
+}
+
+setTimeout(() => {
+    key()
+    canvas()
+    drawSnake()
+}, 200);
+
+setTimeout(() => {
+    key()
+    canvas()
+    drawSnake()
+}, 300);
+
+setTimeout(() => {
+    key()
+    canvas()
+    drawSnake()
+}, 400);
+
+document.addEventListener('',()=>{
+    setTimeout(() => {
+        key(0,10)
+        canvas()
+        drawSnake()
+    }, 400);
+    
+})
+
